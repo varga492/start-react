3edcad4ac49967f916850db24fe91ef64e3a01c1
varga492 <varga492@gmail.com>
2022-09-09 17:13:44 +0200

    session done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Pizza from "./components/Pizza";
 
 function App() {
+  const pizzas = ["Songoku", "Kolbászos rájás", "Csilis babos"];
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +11,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>HI</h1>
+        {pizzas.map((pizza) => (
+          <Pizza name={pizza} />
+        ))}
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Pizza.js
@@ -0,0 +1,12 @@
+import "./Pizza.css";
+
+function Pizza({ name }) {
+  console.log("pizzácskák mindenhol");
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  );
+}
+
+export default Pizza;
